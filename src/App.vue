4814c42d04669d0da98fc229ca51c9df338c775e
ItsDia <1711949109@qq.com>
2024-05-16 21:03:20 +0800

    What can i say,man!

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,71 @@
 <template>
-  <div id="app" class="container" @wheel="handleScroll">
-    <div
-      v-for="(card, index) in cards"
-      :key="card.id"
-      class="card"
-      :style="{
-        zIndex: card.zIndex,
-        transform: `translateZ(${card.zIndex === maxZIndex ? 0 : -100}px)`
-      }"
-    >
-      <img :src="card.imageUrl" :alt="`Image ${index + 1}`" class="card-image" />
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-menu mode="horizontal">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">关于我们</el-menu-item>
+          <el-menu-item index="3">联系我们</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <el-carousel height="60vh">
+          <el-carousel-item @click="goToDetails">
+            <h3>计算机科学与技术学院</h3>
+          </el-carousel-item>
+          <el-carousel-item @click="goToDetails">
+            <h3>电气工程及其自动化学院</h3>
+          </el-carousel-item>
+          <el-carousel-item @click="goToDetails">
+            <h3>车辆工程学院</h3>
+          </el-carousel-item>
+          <el-carousel-item @click="goToDetails">
+            <h3>食品工程学院</h3>
+          </el-carousel-item>
+          <el-carousel-item @click="goToDetails">
+            <h3>建筑工程学院</h3>
+          </el-carousel-item>
+          <el-carousel-item @click="goToDetails">
+            <h3>机械工程学院</h3>
+          </el-carousel-item>
+
+        </el-carousel>
+      </el-main>
+      <el-footer>© 2024 YOUTH OL</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      //nodejs
-      cards: require('./components/text/cards.json'),
-      maxZIndex: 1,
-    };
-  },
+  name: 'App',
   methods: {
-    handleScroll(event) {
-      const direction = event.deltaY > 0 ? 1 : -1;
-
-      this.cards.forEach(card => {
-        card.zIndex += direction;
-
-        if (card.zIndex > this.maxZIndex) {
-          card.zIndex = 0;
-        } else if (card.zIndex < 0) {
-          card.zIndex = this.maxZIndex;
-        }
-      });
-
-      this.cards.sort((a, b) => b.zIndex - a.zIndex);
+    goToDetails() {
+      // TODO :how to jump to details/index.php
+      window.location.href = 'details/index.php';
     }
   }
-};
+}
 </script>
 
-<style>
-.container {
-  position: relative;
-  width: 1920px;
-  height: 1080px;
-  perspective: 1000px;
+<style scoped>
+.demonstration {
+  color: var(--el-text-color-secondary);
 }
 
-.card {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  transition: transform 0.5s, z-index 0s;
-  top: 0;
-  left: 0;
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
 }
 
-.card-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
