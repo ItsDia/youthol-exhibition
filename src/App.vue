9371a6da9a676cea9b81728ec4888e4fd9383d6b
Ji Yeshang <81836540+CSBigCaptain@users.noreply.github.com>
2024-07-03 20:06:14 +0800

    1. 增加主页深色模式支持以及颜色切换 2. 顶部首页样式重新设计 3. 首页js部分使用setup语法糖重写 4. 其他细节

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@
     <el-container>
       <el-header>
         <el-menu mode="horizontal">
-          <el-menu-item index="1">首页</el-menu-item>
-          <el-menu-item index="2">关于我们</el-menu-item>
-          <el-menu-item index="3">联系我们</el-menu-item>
+          <main-page-topic></main-page-topic>
+<!--          <div style="flex-grow: 1"></div> 这里注释掉是因为打算将按钮放到右侧，如果用这个语句会导致右侧按钮自动被折叠，暂无解决方法-->
+          <div style="display: flex; align-items: center;">
+            <el-button type="primary"><el-link type="default" href="https://youth.sdut.edu.cn">返回青春在线</el-link></el-button>
+            <el-button type="primary" @click="toggleDark()">{{ isDark === true ? "浅色" : "深色" }}模式</el-button>
+          </div>
         </el-menu>
       </el-header>
       <el-main>
-        <el-carousel height="60vh">
+        <el-carousel height="70vh" motion-blur arrow="always">
           <el-carousel-item @click="goToDetails">
             <h3>计算机科学与技术学院</h3>
           </el-carousel-item>
@@ -23,34 +26,38 @@
             <h3>农业工程与食品科学学院</h3>
           </el-carousel-item>
           <el-carousel-item @click="goToDetails">
-            <h3>建筑工程学院</h3>
+            <h3>建筑工程与空间信息学院</h3>
           </el-carousel-item>
           <el-carousel-item @click="goToDetails">
             <h3>机械工程学院</h3>
           </el-carousel-item>
-
         </el-carousel>
       </el-main>
-      <el-footer>© 2024 YOUTH OL</el-footer>
+      <el-footer>Youthol © 2001-2024 Created by CSBigCaptain and ItsDia</el-footer>
     </el-container>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  methods: {
-    goToDetails() {
-      // TODO :how to jump to details/index.php
-      window.location.href = 'details/index.php';
-    }
-  }
+<script setup>
+import MainPageTopic from "@/components/MainPageTopic.vue";
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+const goToDetails = function () {
+  // TODO :how to jump to details/index.php
+  window.location.href = 'details/index.php'
 }
 </script>
 
 <style scoped>
 .demonstration {
   color: var(--el-text-color-secondary);
+}
+
+.el-menu .el-button a{
+  color: white;
 }
 
 .el-carousel__item h3 {
@@ -67,5 +74,9 @@ export default {
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+
+.el-footer {
+  border-top: #4C4D4F 2rpx solid;
 }
 </style>
---
+++ src/components/MainPageTopic.vue
@@ -0,0 +1,19 @@
+<!-- 网页标题组件 -->
+<script setup>
+
+</script>
+
+<template>
+  <li class="main">
+    <h1>山东理工大学辅导员风采</h1>
+  </li>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  padding: 20px;
+}
+
+</style>
